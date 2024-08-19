f389acaff243cde611919c20711c3ab39d33324b
[CLOUD4] 이하현 <lovehh329@naver.com>
2024-08-20 01:34:04 +0900

    chore: main에서 테스트 코드 제거 및 git 추적사항 변경

--- src/main/java/com/team3/Team3Application.java
+++ src/main/java/com/team3/Team3Application.java
@@ -1,25 +1,13 @@
 package com.team3;
 
-import com.team3.global.service.DatabaseTestService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import jakarta.annotation.PostConstruct;
-
 @SpringBootApplication
 public class Team3Application {
-
-	@Autowired
-	private DatabaseTestService databaseTestService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Team3Application.class, args);
 	}
 
-	@PostConstruct
-	public void init() {
-		// 애플리케이션이 시작된 후 데이터베이스 연결을 테스트합니다.
-		databaseTestService.testConnection();
-	}
 }
