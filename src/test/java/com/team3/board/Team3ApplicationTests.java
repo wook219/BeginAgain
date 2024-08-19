64c458654d4a6910f343bf146340a7b8324c4c43
[CLOUD4] 송진욱 <wlsdnr19731@gmail.com>
2024-08-19 19:55:38 +0900

    team3 project init !!

---
+++ src/test/java/com/team3/board/Team3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team3.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
