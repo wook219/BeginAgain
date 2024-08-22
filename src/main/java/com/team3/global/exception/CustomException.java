b8103289025e93eea025e0752034a027876ea18a
[CLOUD4] 이하현 <lovehh329@naver.com>
2024-08-22 14:16:49 +0900

    feat : global.exception 패키지에 error처리 로직 추가

---
+++ src/main/java/com/team3/global/exception/CustomException.java
@@ -0,0 +1,10 @@
+package com.team3.global.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class CustomException extends RuntimeException{
+    ErrorCode errorCode;
+}
