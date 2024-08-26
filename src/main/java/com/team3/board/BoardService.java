23636e2ea8ed09994dc056741ff9c45c3512effc
[CLOUD4] 이하현 <lovehh329@naver.com>
2024-08-26 14:57:29 +0900

    feat : BoardService.java 에서 반복적인 게시판 조회 코드를 줄이기 위해 공통 메서드 추가

--- src/main/java/com/team3/board/BoardService.java
+++ src/main/java/com/team3/board/BoardService.java
@@ -1,30 +1,26 @@
 package com.team3.board;
 
-import org.hibernate.query.Page;
+import com.team3.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
-import org.springframework.jdbc.support.CustomSQLExceptionTranslatorRegistrar;
 import org.springframework.stereotype.Service;
 
-import java.awt.print.Pageable;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Optional;
 
 @Service
 public class BoardService {
     private final BoardRepository boardRepository;
+    private final UserService userService;
 
     @Autowired
-    public BoardService(BoardRepository boardRepository) {
+    public BoardService(BoardRepository boardRepository, UserService userService) {
         this.boardRepository = boardRepository;
+        this.userService = userService;
     }
 
     // 게시판 생성
-    public BoardEntity addBoard(CreateBoardDto createBoardDto){
-
+    public BoardEntity addBoard(CreateBoardDto createBoardDto) {
         BoardEntity newBoard = new BoardEntity(
                 null, // id는 자동 생성
                 createBoardDto.getTitle(),
@@ -37,55 +33,41 @@ public class BoardService {
         );
 
         boardRepository.save(newBoard);
-
         return newBoard;
     }
 
-    // TODO : 게시판 상세 조회
-    // 게시판 상세 조회
-    public BoardEntity getBoard(Integer boardId) {
-        // 기존 게시판 조회
-        BoardEntity existingBoard = boardRepository.findById(boardId)
+    // 반복적인 게시판 조회 코드를 줄이기 위해 공통 메서드 추가
+    private BoardEntity findExistingBoard(Integer boardId) {
+        BoardEntity board = boardRepository.findById(boardId)
                 .orElseThrow(() -> new NoSuchElementException("게시판을 찾을 수 없습니다."));
-        // 기존 게시판이 삭제되었는지 확인
-        if (existingBoard.isDeleted()) {
-            // 삭제된 게시글이면 예외를 던집니다.
+
+        if (board.isDeleted()) {
             throw new IllegalStateException("게시판이 삭제되었습니다.");
-        } else {
-            // 삭제되지 않은 게시판이면 해당 게시글 반환
-            return existingBoard;
         }
+
+        return board;
     }
 
-//    // 삭제되지 않은 게시판 조회 및 페이지네이션
-//    public Page<BoardEntity> getAllBoards(int page, int size) {
-//        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "createdAt"));
-//        return boardRepository.findByDeletedAtIsNull(pageable);
-//    }
+    // 게시판 상세 조회
+    public BoardEntity getBoard(Integer boardId) {
+        return findExistingBoard(boardId);
+    }
 
-    // TODO : 게시판 전체 (리스트) 조회
-    // 삭제되지 않은 모든 게시판 조회
+    // 게시판 전체 (리스트) 조회
     public List<BoardEntity> getAllBoards() {
         return boardRepository.findByDeletedAtIsNullOrderByCreatedAtDesc();
     }
 
-    // TODO : 게시판 수정
+    // 게시판 수정
     public BoardEntity updateBoard(Integer boardId, UpdateBoardDTO updateBoardDTO) {
         // TODO : 현재 접속 중인 사용자 정보 조회
 //        UserDocument currentUser = userService.getCurrentUser();
 
-        // 기존 게시판 조회
-        BoardEntity existingBoard = boardRepository.findById(boardId)
-                .orElseThrow(() -> new NoSuchElementException("게시판을 찾을 수 없습니다."));
-
-        // 기존 게시판이 삭제되었는지 확인
-        if (existingBoard.isDeleted()) {
-            // 삭제된 게시글이면 예외를 던집니다.
-            throw new IllegalStateException("게시판이 삭제되었습니다.");
-        }
+        // 공통 메서드를 사용하여 기존 게시판 조회
+        BoardEntity existingBoard = findExistingBoard(boardId);
 
         // TODO : 현재 사용자가 작성자인지 확인
-//        if (! existingBoard.getAuthor().getId().equals(currentUser.getId())) {
+//        if (!existingBoard.getAuthor().getId().equals(currentUser.getId())) {
 //            throw new CustomException(USER_NOT_AUTHENTICATED);
 //        }
 
@@ -102,19 +84,17 @@ public class BoardService {
 
         // 수정된 게시판 저장
         return boardRepository.save(existingBoard);
-
     }
 
-    // TODO : 게시판 삭제
+    // 게시판 삭제
     public BoardEntity deleteBoard(Integer boardId) {
         // TODO : 현재 접속 중인 사용자 정보 조회
 
-        // 기존 게시판 조회
-        BoardEntity existingBoard = boardRepository.findById(boardId)
-                .orElseThrow(() -> new NoSuchElementException("게시판을 찾을 수 없습니다."));
+        // 공통 메서드를 사용하여 기존 게시판 조회
+        BoardEntity existingBoard = findExistingBoard(boardId);
 
         // TODO : 현재 사용자가 작성자인지 확인
-//        if (!existingBoard.getAuthor().getId().equals(currnetUser.getId())) {
+//        if (!existingBoard.getAuthor().getId().equals(currentUser.getId())) {
 //            throw new CustomException(USER_NOT_AUTHENTICATED);
 //        }
 
@@ -123,5 +103,4 @@ public class BoardService {
 
         return boardRepository.save(existingBoard);
     }
-
 }
