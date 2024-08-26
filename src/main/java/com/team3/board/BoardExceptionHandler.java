1b39401a84cd24a65d74126ad77850a080e5b0b3
jsjhyun <gusrnl98@naver.com>
2024-08-27 02:32:25 +0900

    fix: mypage 404 오류 해결

--- src/main/java/com/team3/board/BoardExceptionHandler.java
+++ src/main/java/com/team3/board/BoardExceptionHandler.java
@@ -1,32 +1,32 @@
-package com.team3.board;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.util.NoSuchElementException;
-
-@ControllerAdvice
-public class BoardExceptionHandler {
-
-    @ExceptionHandler(NoSuchElementException.class)
-    public ModelAndView handleNoSuchElementException(NoSuchElementException e) {
-        ModelAndView mav = new ModelAndView("error/404");
-        mav.setStatus(HttpStatus.NOT_FOUND);
-        return mav;
-    }
-
-    @ExceptionHandler(IllegalStateException.class)
-    public ModelAndView handleIllegalStateException(IllegalStateException e) {
-        ModelAndView mav = new ModelAndView("error/410");
-        mav.setStatus(HttpStatus.GONE);
-        return mav;
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleGenericException(Exception e) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while processing your request.");
-    }
-}
+//package com.team3.board;
+//
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.ControllerAdvice;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//import org.springframework.web.servlet.ModelAndView;
+//
+//import java.util.NoSuchElementException;
+//
+//@ControllerAdvice
+//public class BoardExceptionHandler {
+//
+//    @ExceptionHandler(NoSuchElementException.class)
+//    public ModelAndView handleNoSuchElementException(NoSuchElementException e) {
+//        ModelAndView mav = new ModelAndView("error/404");
+//        mav.setStatus(HttpStatus.NOT_FOUND);
+//        return mav;
+//    }
+//
+//    @ExceptionHandler(IllegalStateException.class)
+//    public ModelAndView handleIllegalStateException(IllegalStateException e) {
+//        ModelAndView mav = new ModelAndView("error/410");
+//        mav.setStatus(HttpStatus.GONE);
+//        return mav;
+//    }
+//
+//    @ExceptionHandler(Exception.class)
+//    public ResponseEntity<String> handleGenericException(Exception e) {
+//        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while processing your request.");
+//    }
+//}
