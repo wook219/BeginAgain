a05f5c2e92a584df2d6f0912969f4bbf78078536
[CLOUD4] 송진욱 <wlsdnr19731@gmail.com>
2024-08-27 20:37:57 +0900

    feat : 댓글 CRUD 기능 추가

--- src/main/java/com/team3/comment/repository/CommentRepository.java
+++ src/main/java/com/team3/comment/repository/CommentRepository.java
@@ -4,7 +4,10 @@ import com.team3.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
     // JpaRepository를 상속받아 기본 CRUD 메서드를 사용
+    List<Comment> findByPostId(Integer postId);
 }
