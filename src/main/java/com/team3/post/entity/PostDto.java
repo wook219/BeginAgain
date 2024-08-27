41a72683b8acbbade49ecaea13a2283afcc7cec1
[CLOUD4] 송진욱 <wlsdnr19731@gmail.com>
2024-08-27 20:37:24 +0900

    feat : 페이징 관련 생성자 추가

--- src/main/java/com/team3/post/entity/PostDto.java
+++ src/main/java/com/team3/post/entity/PostDto.java
@@ -1,12 +1,15 @@
 package com.team3.post.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.sql.Timestamp;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class PostDto {
     private Integer postId;
     private String title;
@@ -16,4 +19,19 @@ public class PostDto {
     private Integer boardId;
     private Timestamp createdAt;
     private Timestamp updatedAt;
+
+    public PostDto(PostEntity postEntity) {
+        this.postId = postEntity.getPostId();
+        this.title = postEntity.getTitle();
+        this.content = postEntity.getContent();
+        this.views = postEntity.getViews();
+        this.userId = postEntity.getUserId();
+        this.boardId = postEntity.getBoardId();
+        this.createdAt = postEntity.getCreatedAt();
+        this.updatedAt = postEntity.getUpdatedAt();
+    }
 }
+
+
+
+
