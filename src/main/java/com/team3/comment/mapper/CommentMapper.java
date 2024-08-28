3e97383f5d55daffd7191fd51eb366e2fd743f08
[CLOUD4] 송진욱 <wlsdnr19731@gmail.com>
2024-08-29 00:10:00 +0900

    delete : mapper 추후 리팩토링하며 추가 예정이므로 일시 삭제

--- src/main/java/com/team3/comment/mapper/CommentMapper.java
+++
@@ -1,9 +0,0 @@
-package com.team3.comment.mapper;
-
-import com.team3.comment.dto.CommentDto;
-import com.team3.comment.entity.Comment;
-
-public interface CommentMapper {
-    CommentDto toDto(Comment comment);
-    Comment toEntity(CommentDto commentDto);
-}
