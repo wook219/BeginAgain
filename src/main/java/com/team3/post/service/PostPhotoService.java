1a474f155ad018c9656a457d48749336742f80c8
[CLOUD4] 송진욱 <wlsdnr19731@gmail.com>
2024-09-20 14:36:03 +0900

    Fix : Entity타입의 반환타입을 Dto타입으로 변경

--- src/main/java/com/team3/post/service/PostPhotoService.java
+++ src/main/java/com/team3/post/service/PostPhotoService.java
@@ -1,20 +1,34 @@
 package com.team3.post.service;
 
+import com.team3.post.entity.PostPhotoDto;
 import com.team3.post.entity.PostPhotoEntity;
+import com.team3.post.mapper.PostPhotoMapper;
 import com.team3.post.repository.PostPhotoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class PostPhotoService {
+
+    @Autowired
+    PostPhotoMapper postPhotoMapper;
+
     private final PostPhotoRepository postPhotoRepository;
 
     public PostPhotoService(PostPhotoRepository postPhotoRepository) {
         this.postPhotoRepository = postPhotoRepository;
     }
 
-    public List<PostPhotoEntity> getPhotosByPostId(Integer postId){
-        return postPhotoRepository.findByPost_PostId(postId);
+    public List<PostPhotoDto> getPhotosByPostId(Integer postId){
+        List<PostPhotoEntity> posts = postPhotoRepository.findByPost_PostId(postId);
+        List<PostPhotoDto> postDtos = new ArrayList<>();
+
+        for (PostPhotoEntity post : posts) {
+            postDtos.add(postPhotoMapper.toPostPhotoDto(post));
+        }
+        return postDtos;
     }
 }
--- src/main/java/com/team3/post/service/PostService.java
+++ src/main/java/com/team3/post/service/PostService.java
@@ -166,11 +166,11 @@ public class PostService {
     }
 
     //postId에 따른 게시글 단건 조회
-    public PostEntity getPostByPostId(Integer postId) {
+    public PostDto getPostByPostId(Integer postId) {
         PostEntity post = postRepository.findById(postId)
                 .orElseThrow(() -> new NoSuchElementException("게시글을 찾을 수 없습니다."));
 
-        return post;
+        return postMapper.toPostDto(post);
     }
 
     //게시글 수정
