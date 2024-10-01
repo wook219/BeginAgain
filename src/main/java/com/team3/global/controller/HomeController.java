979550e0b158b8bd59ac7ef3a3409946329d0c29
[CLOUD4] 송진욱 <wlsdnr19731@gmail.com>
2024-10-01 23:16:02 +0900

    feat : localhost:8080 으로 접속시 메인페이지 뜨도록 설정

--- src/main/java/com/team3/global/controller/HomeController.java
+++ src/main/java/com/team3/global/controller/HomeController.java
@@ -1,13 +1,34 @@
 package com.team3.global.controller;
 
+import com.team3.board.BoardEntity;
+import com.team3.board.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    BoardService boardService;
+
     @GetMapping
-    public String home(){
-        return "home";
+    public String home(Model model,
+                       @RequestParam(value = "page", defaultValue = "0") int page,
+                       @RequestParam(value = "size", defaultValue = "9") int size,
+                       @RequestParam(value = "sortField", defaultValue = "createdAt") String sortField,
+                       @RequestParam(value = "sortDir", defaultValue = "desc") String sortDir,
+                       @RequestParam(value = "keyword", required = false) String keyword) {
+        Page<BoardEntity> boards = boardService.getBoards(page, size, sortField, sortDir, keyword);
+        model.addAttribute("boards", boards.getContent());
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", boards.getTotalPages());
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortDir", sortDir);
+        model.addAttribute("keyword", keyword); // 검색 키워드를 모델에 추가
+        return "board/listBoards";
     }
 }
